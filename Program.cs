e8e8e7a181d3bd165b4f811adee140fc03b02812
Devania Chetty <ST10049331@vcconnect.edu.za>
2024-10-05 21:00:07 +0200

    Main Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPointer
+{
+    public  class Program
+    {
+        static unsafe void Main(string[] args) // click on lightbulb -> allow unsafe code into this project
+        {
+            //variables
+            int a = 20;
+            int b = 50;
+
+            //create a pointer --> point to a 
+            int* pointerA = &a;
+            int* pointerB = &b;
+
+            //output
+            Console.WriteLine(*pointerA);
+            Console.WriteLine((int)pointerA);
+            Console.WriteLine(*pointerB);
+            Console.WriteLine((int)pointerB);
+
+            Console.ReadLine();
+
+        }
+    }
+}
